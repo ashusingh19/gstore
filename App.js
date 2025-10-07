ca06094c701ef6d726871662294ebb9a26466205
ashusingh19 <singhashu19112003@gmail.com>
2025-10-07 11:38:29 +0530

    change app file

---
+++ App.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize:35,color:"black"}}>App</Text>
+    </View>
+  )
+}
+
+export default App
